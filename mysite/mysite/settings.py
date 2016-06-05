df9ddeff3b04da4f3bee90825386d44cb0855bfa
Javier Onglao <javier@juicero.com>
2016-06-04 21:39:21 -0700

    Get back the effing migrations

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'mysite.urls'
@@ -78,13 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'RSDTrailers',
-	'USER': 'Jeanwoo',
-	'PASSWORD': 'jh012490',
-	'HOST': 'localhost',
-	'PORT': '5000'
+        'NAME': 'mydb',
+        'USER': 'mydb_user',
+        'PASSWORD': 'your_password',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 
 # Internationalization
