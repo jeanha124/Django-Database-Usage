fe0ea7bc92d47a39b9380597d67c8c5a73c08c23
Jean Woo Ha <jeanha124@gmail.com>
2016-06-20 10:38:04 -0400

    changed to mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
 
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'rsdtrailers',
 	    'USER': 'Jeanwoo',
 	    'PASSWORD': 'jh012490',
