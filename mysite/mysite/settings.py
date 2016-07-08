d427f4faa375ab3b0028498d31f6f8766019272f
Javier Onglao <javier@juicero.com>
2016-07-08 11:47:56 -0700

    Fix Heroku deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,6 +15,10 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
